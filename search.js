b4930522ece4f0cb7e8361b62d925e05d1cc3e38
anagh1008 <73767403+anagh1008@users.noreply.github.com>
2022-11-06 19:26:37 -0800

    Update search.js

--- search.js
+++ search.js
@@ -20,6 +20,30 @@ function parse_query()
         window.location.href = "https://en.wikipedia.org/wiki/" + query[1];
 
     }
+    else if (query[0] === "links")
+    {
+        window.location.href = "https://docs.google.com/spreadsheets/d/1KXgH2cUDro-u5Zk7CmImQnbPp8bP2__Q031gG048qOs/edit?usp=sharing";
+
+    }
+      else if (query[0] === "csu")
+    {
+        window.location.href = "https://www.calstate.edu/apply";
+
+    }
+      else if (query[0] === "uc")
+    {
+        window.location.href = "https://apply.universityofcalifornia.edu/my-application/reviewSubmit";
+
+    }
+       else if (query[0] === "commonap")
+    {
+        window.location.href = "https://apply.commonapp.org/dashboard";
+
+    }
+       else if (query[0] === "counselmore")
+    {
+        window.location.href = "https://app.counselmore.com/student/tasks";
+    }
     else
     {
         let query_string = query.join("+");
